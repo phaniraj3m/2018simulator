98c68d56e3361bc731743c084aeaf8cd285ceabf
Raj Phaniraj <raj.phaniraj@gmail.com>
2018-01-22 12:41:38 -0600

    Cleanup folder

--- Sim2/src/org/usfirst/frc/team2175/simulator/ui/BlueSprite.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team2175.simulator.ui;
-
-public class BlueSprite extends Sprite {
-
-}
--- Sim2/src/org/usfirst/frc/team2175/simulator/ui/RedSprite.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team2175.simulator.ui;
-
-public class RedSprite extends Sprite {
-
-}
